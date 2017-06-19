098c3b8f3249cc4f6d41f82810dc744a1a696cae
Jay Ryu <bluejay9676@gmail.com>
2017-06-19 00:05:56 -0500

    Call to the kernel in progress.

--- clMatrixMul.cpp
+++ clMatrixMul.cpp
@@ -11,32 +11,6 @@ int Bucket::_size = 64; //use more if the buckets are heavy
 
 using namespace std;
 
-
-__kernel
-void divideRow(int Mdim, int Ndim, int Pdim, 
-    __global double* A,
-    __global double* B,
-    __global double* C, 
-    __global LSH* _Algo,
-    __global SignedRandomProjection* proj)
-{
-    //Run vector multiplication for each columns from the retrieved LSH bucket.
-    int i = get_global_id(0); // Index of a row of A
-    int *queryhashes = proj->getHash(&A[i * Pdim], Pdim);
-    int *retrieved = _Algo->retrieve(queryhashes);//Indices of bucket that collided.
-
-    if (i < Ndim)
-    {
-        for (int l = 2; l < retrieved[0] + 2; l++)
-        {
-            for (int k = 0; k < Pdim; k++)
-            {
-                C[i * Mdim + retrieved[l] - 1] += A[i * Ndim + k] * B[k * Pdim + retrieved[l] - 1];
-            }
-        }
-    }
-}
-
 void parMatMul(int K, int L, double* A, double* B, double* C, int N, int M, int P)
 {
     //Create LSH (Assuming that this runs in parallel) =============================
@@ -56,7 +30,8 @@ void parMatMul(int K, int L, double* A, double* B, double* C, int N, int M, int 
         _Algo->add(hashes, c + 1);
     }
     //=============================================================================== 
-    
+
+    //Run each row of A in parallel.
     //Divide into N
     divideRow(M, N, P, (double *) A, (double *) B, (double *) C, (LSH *) _Algo, (SignedRandomProjection *) proj);
 }
